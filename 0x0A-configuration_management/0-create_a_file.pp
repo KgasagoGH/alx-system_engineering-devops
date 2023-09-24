3429fff8fe025f2195f2afb8c0f11a7d1f47c054
KgasagoGH <130768415+KgasagoGH@users.noreply.github.com>
2023-09-25 00:16:15 +0200

    Add files via upload

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,7 @@
--rwxr--r-- 1 www-data www-data
+# Create a file in /tmp
+file {'/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
